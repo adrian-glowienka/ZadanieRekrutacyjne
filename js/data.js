fb3785d9832fde44117f3a3030937278022c6feb
adrian-glowienka <45940806+adrian-glowienka@users.noreply.github.com>
2019-10-29 10:53:02 +0100

    new code

--- js/data.js
+++ js/data.js
@@ -1,51 +1,10 @@
-// show the shows with highest ranking
-fetch('https://api.tvmaze.com/shows')
-    .then(blob => blob.json())
-    .then(json => {
-        const topTenShows = json
-        .filter(show => show.rating.average) 
-        .sort((a, b) => a.rating.average > b.rating.average ? -1 : 1) 
-        .slice(0, 9) // tar element 0-9 i arrayen
-
-    return topTenShows
-  })
-  .then(shows => {
-    const app = document.getElementById('app')
-
-    app.innerHTML = shows.map(show => `
-      <div class="col-sm movie-content">
-        <img src="${show.image.medium}">
-        <div class="movie-info">
-          <h5>${show.name}</h5>
-          <span>Rating: ${show.rating.average}</span>
-          <br />
-          <span>Rating: ${show.genres}</span>
-        </div>
-      </div>
-
-    `).join()
-  })
-
-// show the search results
-function searchTvAmaze ({ target }) {
-    fetch(`https://api.tvmaze.com/search/shows?q=${target.value}`)
-        .then(blob => blob.json())
-        .then(shows => {
-            const app = document.getElementById('app')
-        app.innerHTML = shows.map(({ show }) => `
-          <div class="col-sm movie-content">
-            ${show.image ? `<img src="${show.image.medium}">` : ''}
-            <div class="movie-info">
-              <h5>${show.name}</h5>
-              <span>Rating: ${show.rating.average}</span>
-              <br />
-              <span>Rating: ${show.genres}</span>
-            </div>
-          </div>
-
-        `).join();
-      })
-  }
-
-  const inputSearchField = document.querySelector('.inputSearchField')
-  inputSearchField.addEventListener('keydown', searchTvAmaze)
+const requestData = (url, handler) =>
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Response is not OK");
+    })
+    .then(handler)
+    .catch(error => console.log(error.message));
