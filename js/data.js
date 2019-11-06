72d80ed1a84c70046a562271ea221716c257281f
adrian-glowienka <45940806+adrian-glowienka@users.noreply.github.com>
2019-11-06 15:48:00 +0100

    unexpected token

--- js/data.js
+++ js/data.js
@@ -1,3 +1,10 @@
+// const details = document.querySelector("#details__show");
+const list = document.querySelector("#shows");
+
+
+
+
+
 const requestData = (url, handler) =>
   fetch(url)
     .then(response => {
@@ -7,4 +14,28 @@ const requestData = (url, handler) =>
       throw new Error("Response is not OK");
     })
     .then(handler)
-    .catch(error => console.log(error.message));
+    .catch(error => console.log(error.message))
+
+
+
+
+    const showDetails = details => {
+      name.innerHTML = `Name: ${details.name}`;
+    };
+
+
+    const createList = data => {
+      data.map(show => {
+        const listItem = document.createElement("li");
+          listItem.innerText = show.name;
+          listItem.addEventListener("click", function(event) {
+          const urlShow = show.url
+          requestData(urlShow, showDetails);
+          });
+
+          return listItem;
+        }).slice(0,12)
+        .forEach(item => list.appendChild(item));
+    };
+
+    requestData("https://api.tvmaze.com/shows", createList);
