08752b763dfd84054dc76b6cf52d48a53998c8f1
adrian-glowienka <adisonn88@wp.pl>
2019-12-19 14:06:54 +0100

    display content with click

--- js/data.js
+++ js/data.js
@@ -1,41 +1,44 @@
 // const details = document.querySelector("#details__show");
 const list = document.querySelector("#shows");
+const API_URL = 'https://api.tvmaze.com';
 
+const name = document.querySelector("#name");
+const score = document.querySelector("#score");
+const relaseDate = document.querySelector("#relase-date");
+const runtime = document.querySelector("#runtime");
+const description = document.querySelector("#description");
 
-
-
-
-const requestData = (url, handler) =>
+const requestData = (url, handler) => {
   fetch(url)
     .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error("Response is not OK");
+      return response.json()
     })
-    .then(handler)
+    .then((data) => handler(data))
     .catch(error => console.log(error.message))
+}
+
+const showDetails = details => {
+  console.log(details);
+
+  name.innerHTML = `Name: ${details.name}`;
+  score.innerHTML = `Score: ${details.rating.average}`;
+  relaseDate.innerHTML = `Relase date: ${details.premiered}`;
+  runtime.innerHTML = `Runtime: ${details.runtime}`;
+  description.innerHTML = `Description: ${details.summary}`;
+};
 
 
+const createList = data => {
 
+  data.map(show => {
+      const listItem = document.createElement("li");
+      listItem.innerText = show.name;
+      listItem.addEventListener("click", function () {
+        requestData(`${API_URL}/shows/${show.id}`, showDetails);
+      });
+      return listItem;
+    }).slice(0, 12)
+    .forEach(item => list.appendChild(item));
+};
 
-    const showDetails = details => {
-      name.innerHTML = `Name: ${details.name}`;
-    };
-
-
-    const createList = data => {
-      data.map(show => {
-        const listItem = document.createElement("li");
-          listItem.innerText = show.name;
-          listItem.addEventListener("click", function(event) {
-          const urlShow = show.url
-          requestData(urlShow, showDetails);
-          });
-
-          return listItem;
-        }).slice(0,12)
-        .forEach(item => list.appendChild(item));
-    };
-
-    requestData("https://api.tvmaze.com/shows", createList);
+requestData(`${API_URL}/shows`, createList);
